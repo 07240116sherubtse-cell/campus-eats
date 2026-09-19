0b4980b9beb73b596a3707ba89ac605cf7f53fe5
07240116sherubtse-cell <07240116.sherubtse@rub.edu.bt>
2026-09-19 14:19:26 +0600

    Unit IV Lab 4: Order model, full CRUD, validation, aggregation, and transactions

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 const aboutController = require('../controllers/aboutController');
-const menuController = require('../controllers/menuController');
 const orderController = require('../controllers/orderController');
 
 router.post('/orders', orderController.createOrder);
-router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant); 
+router.get('/orders/:id', orderController.getOrder);
 router.get('/', homeController.getHome);
 router.get('/about', aboutController.getAbout);
+router.post('/orders/:id/cancel', orderController.cancelOrder);
+
+const menuController = require('../controllers/menuController');
+
+router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant);
+
 
 module.exports = router;
+
+router.post('/orders/:id/update', orderController.updateOrder);
 
